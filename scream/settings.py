b09a2e4ac195632a0f0640b11f0572601d439b87
Evgeniy Gerbin <zenagerbin@gmail.com>
2022-01-25 22:28:57 +0200

    databases

--- scream/settings.py
+++ scream/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'scream.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2-binary',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': '11111111',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
