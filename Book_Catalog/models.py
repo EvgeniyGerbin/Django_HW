2f828cc9978657eea7f77173c82f1bcc42b4fe21
Evgeniy Gerbin <zenagerbin@gmail.com>
2022-01-29 13:26:54 +0200

    create books and authors

--- Book_Catalog/models.py
+++ Book_Catalog/models.py
@@ -1,28 +1,28 @@
 from django.db import models
 
 
-
-
 class Author(models.Model):
     first_name = models.CharField('Name', max_length=32)
     last_name = models.CharField('Surname', max_length=32)
 
     class Meta:
-        verbose_name = 'Authors'
-        verbose_name_plural = 'Author'
+        verbose_name = 'Author'
+        verbose_name_plural = 'Authors'
 
     def __str__(self):
         return f'{self.first_name}  {self.last_name}'
 
 
 class Book(models.Model):
-    title = models.CharField("Book Title", max_length=32)
-    author = models.OneToOneField('Book_Catalog.Author', on_delete=models.CASCADE)
+    title = models.CharField('Book Title', max_length=32)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books', verbose_name='Author')
 
     class Meta:
-        verbose_name = "Books"
-        verbose_name_plural = 'Book'
+        verbose_name = "Book"
+        verbose_name_plural = 'Books'
 
     def __str__(self):
         return f'{self.title} by {self.author}'
+
+
 
