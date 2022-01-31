b388e5fd74db7f6c8267acc77888c51961cf8c3d
Evgeniy Gerbin <zenagerbin@gmail.com>
2022-01-31 19:13:41 +0200

    create Blog and Library

--- blog/admin.py
+++ blog/admin.py
@@ -2,10 +2,36 @@ from django.contrib import admin
 from Blog.models import *
 
 
-admin.site.register(Bloger)
-admin.site.register(Post)
-admin.site.register(CommentPost)
-admin.site.register(LikeComment)
-admin.site.register(DisslikeComment)
-admin.site.register(LikePost)
-admin.site.register(DisslikePost)
+@admin.register(Bloger)
+class BlogerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CommentPost)
+class CommentPostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(LikeComment)
+class LikeCommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DisslikeComment)
+class DisslikeCommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(LikePost)
+class LikePostAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DisslikePost)
+class DisslikePostAdmin(admin.ModelAdmin):
+    pass
